ea275497a6d37233192724f9df246eafb3cf2515
brendan_m_nolan <bnolan23@gmail.com>
2022-09-05 13:07:30 +0100

    Use read_u64 and write_u64 Methods
    Doing this rather than manually serializing and deserializing a u64.

--- connection_utils/src/stream_handling.rs
+++ connection_utils/src/stream_handling.rs
@@ -8,7 +8,7 @@ use tokio::{
 type ByteArray = Vec<u8>;
 
 struct ByteMessage {
-    size: ByteArray,
+    size: u64,
     data: ByteArray,
 }
 
@@ -31,27 +31,20 @@ impl From<tokio::io::Error> for ReadError {
 
 fn convert_to_byte_message(item: &impl Communicable) -> ByteMessage {
     let data = bincode::serialize(&item).unwrap();
-    let size = bincode::serialize(&(data.len() as u64)).unwrap();
+    let size = data.len() as u64;
     ByteMessage { size, data }
 }
 
 pub async fn send(data: &impl Communicable, writer: &mut WriteHalf<TcpStream>) {
     let byte_message = convert_to_byte_message(data);
-    writer.write_all(&byte_message.size).await.unwrap();
+    writer.write_u64(byte_message.size).await.unwrap();
     writer.write_all(&byte_message.data).await.unwrap();
-}
-
-async fn receive_first_u64(reader: &mut ReadHalf<TcpStream>) -> Result<u64, ReadError> {
-    let mut raw_bytes_received: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
-    reader.read_exact(&mut raw_bytes_received).await?;
-    let deserialized_u64 = bincode::deserialize::<u64>(&raw_bytes_received)?;
-    Ok(deserialized_u64)
 }
 
 pub async fn receive<R: DeserializeOwned>(
     reader: &mut ReadHalf<TcpStream>,
 ) -> Result<R, ReadError> {
-    let num_bytes_to_read = receive_first_u64(reader).await?;
+    let num_bytes_to_read = reader.read_u64().await.unwrap();
     let mut raw_bytes_received = ByteArray::new();
     raw_bytes_received.resize(num_bytes_to_read as usize, 0_u8);
     reader.read_exact(&mut raw_bytes_received).await?;
