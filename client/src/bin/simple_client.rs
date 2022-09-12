2e2a43940653cfc8da5a91de9f49e15af6df67a5
brendan_m_nolan <bnolan23@gmail.com>
2022-09-12 20:03:33 +0100

    Update simple_client
    Now does sequential requests. Now each client process has only one
    client connection.

--- client/src/bin/simple_client.rs
+++ client/src/bin/simple_client.rs
@@ -11,26 +11,25 @@ async fn main() {
     let (tx, rx) = mpsc::channel::<IntStringCommand>(32);
     let stream = TcpStream::connect("127.0.0.1:6379").await.unwrap();
     let manager = tokio::spawn(tasks::create_cyclic_connection_manager(stream, rx));
-    let tx_clone = tx.clone();
-    let client_0 = tokio::spawn(create_client_task(tx));
-    let client_1 = tokio::spawn(create_client_task(tx_clone));
-    client_0.await.unwrap();
-    client_1.await.unwrap();
+    let client = tokio::spawn(create_client_task(tx));
+    client.await.unwrap();
     manager.await.unwrap();
 }
 
 async fn create_client_task(tx: mpsc::Sender<IntStringCommand>) {
-    let (response_tx, response_rx) = oneshot::channel();
-    tx.send(IntStringCommand {
-        data: 147,
-        responder: response_tx,
-    })
-    .await
-    .unwrap();
-    let response = response_rx.await;
-    match response {
-        Ok(Some(response)) => println!("Received a response: {:?}", response),
-        Ok(None) => println!("Failed to read response."),
-        Err(_) => panic!("Client unexpectedly failed to receive a response"),
+    for i in 0..10 {
+        let (response_tx, response_rx) = oneshot::channel();
+        tx.send(IntStringCommand {
+            data: i,
+            responder: response_tx,
+        })
+        .await
+        .unwrap();
+        let response = response_rx.await;
+        match response {
+            Ok(Some(response)) => println!("Received a response: {:?}", response),
+            Ok(None) => println!("Failed to read response."),
+            Err(_) => panic!("Client unexpectedly failed to receive a response"),
+        }
     }
 }
