76b2968ccfaa40c93d5c8bca83d2a5a3e0a46932
brendan_m_nolan <bnolan23@gmail.com>
2022-12-06 17:17:34 +0000

    Update Parameter Name

--- server/src/server_runner.rs
+++ server/src/server_runner.rs
@@ -2,16 +2,16 @@ use crate::{jobs, manage_connection, request_processing::RequestProcessor};
 use connection_utils::{Communicable, TriviallyThreadable};
 use tokio::net::{TcpListener, ToSocketAddrs};
 
-pub async fn run_server<Req: Communicable, Resp: Communicable, Address: ToSocketAddrs, P>(
+pub async fn run_server<Req: Communicable, Resp: Communicable, Address: ToSocketAddrs, Processor>(
     address: Address,
-    p: P,
+    request_processor: Processor,
     jobs_buffer_size: usize,
     read_write_buffer_size: usize,
 ) where
-    P: RequestProcessor<Req, Resp> + TriviallyThreadable,
+    Processor: RequestProcessor<Req, Resp> + TriviallyThreadable,
 {
     let listener = TcpListener::bind(address).await.unwrap();
-    let (job_dispatcher, jobs_task) = jobs::spawn_jobs_task(p, jobs_buffer_size);
+    let (job_dispatcher, jobs_task) = jobs::spawn_jobs_task(request_processor, jobs_buffer_size);
     let mut connection_tasks = Vec::new();
     while let Ok((stream, _)) = listener.accept().await {
         let connection_task = tokio::spawn(manage_connection::create_connection_manager(
