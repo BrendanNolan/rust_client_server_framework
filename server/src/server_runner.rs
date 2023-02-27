92f480d235752777727b3424bfc2c010c7f83e2b
Brendan Nolan <bnolan23@gmail.com>
2023-02-27 20:22:26 +0000

    Refactor Server Runner To Use Helper

--- server/src/server_runner.rs
+++ server/src/server_runner.rs
@@ -1,9 +1,15 @@
 use crate::{
-    jobs, manage_connection, request_processing::RequestProcessor, shutdown::ShutdownListener,
+    jobs::{self, JobDispatcher},
+    manage_connection,
+    request_processing::RequestProcessor,
+    shutdown::ShutdownListener,
 };
 use connection_utils::{Communicable, TriviallyThreadable};
 use futures::future::join_all;
-use tokio::net::{TcpListener, ToSocketAddrs};
+use tokio::{
+    net::{TcpListener, ToSocketAddrs},
+    task::JoinHandle,
+};
 
 pub async fn run_server<
     Req: Communicable,
@@ -13,37 +19,67 @@ pub async fn run_server<
 >(
     address: Address,
     request_processor: Processor,
-    mut shutdown_listener: ShutdownListener,
+    shutdown_listener: ShutdownListener,
     jobs_buffer_size: usize,
     read_write_buffer_size: usize,
 ) {
-    let connection_listener = TcpListener::bind(address).await.unwrap();
     let (job_dispatcher, jobs_task) = jobs::spawn_jobs_task(
         request_processor,
         jobs_buffer_size,
         shutdown_listener.clone(),
     );
+    let connection_tasks = run_server_loop(
+        address,
+        job_dispatcher,
+        read_write_buffer_size,
+        shutdown_listener,
+    )
+    .await;
+    join_all(connection_tasks).await;
+    let _ = jobs_task.await;
+}
+
+async fn run_server_loop<Req: Communicable, Resp: Communicable, Address: ToSocketAddrs>(
+    address: Address,
+    job_dispatcher: JobDispatcher<Req, Resp>,
+    read_write_buffer_size: usize,
+    mut shutdown_listener: ShutdownListener,
+) -> Vec<JoinHandle<()>> {
     let mut connection_tasks = vec![];
+    let connection_listener = TcpListener::bind(address).await.unwrap();
     while !shutdown_listener.is_shutting_down() {
         tokio::select! {
-            connection_result = connection_listener.accept() => {
-                match connection_result {
-                    Ok((stream, _)) => {
-                        println!("Accetped Connection.");
-                        let connection_task = tokio::spawn(manage_connection::create_connection_manager(
-                            stream,
-                            job_dispatcher.clone(),
-                            shutdown_listener.clone(),
-                            read_write_buffer_size,
-                        ));
-                        connection_tasks.push(connection_task);
-                    },
-                    Err(_) => { println!("Failed to accept client connection."); },
+            connection = try_establish_connection(
+                &connection_listener,
+                job_dispatcher.clone(),
+                read_write_buffer_size,
+                shutdown_listener.clone())
+            => {
+                if let Some(connection) = connection {
+                    connection_tasks.push(connection);
+                } else {
+                    println!("Failed to accept client connection.");
                 }
-            },
+            }
             _ = shutdown_listener.listen_for_shutdown_signal() => {},
         }
     }
-    join_all(connection_tasks).await;
-    let _ = jobs_task.await;
+    connection_tasks
+}
+
+async fn try_establish_connection<Req: Communicable, Resp: Communicable>(
+    connection_listener: &TcpListener,
+    job_dispatcher: JobDispatcher<Req, Resp>,
+    read_write_buffer_size: usize,
+    shutdown_listener: ShutdownListener,
+) -> Option<JoinHandle<()>> {
+    let Ok((stream, _)) = connection_listener.accept().await else { return None; };
+    println!("Accetped Connection.");
+    let connection_task = tokio::spawn(manage_connection::create_connection_manager(
+        stream,
+        job_dispatcher,
+        shutdown_listener,
+        read_write_buffer_size,
+    ));
+    Some(connection_task)
 }
