17f1cd72ff532b8a313eaf39bb19b23e88f53645
Brendan Nolan <bnolan23@gmail.com>
2023-06-25 15:33:19 +0100

    Add Simpler Connection Manager Which Is Not Cyclic

--- client/src/tasks.rs
+++ client/src/tasks.rs
@@ -1,6 +1,30 @@
 use crate::command::Command;
 use connection_utils::{stream_serialization, Communicable, ServerError};
-use tokio::{io, net::TcpStream, sync::mpsc::Receiver};
+use tokio::{
+    io,
+    net::TcpStream,
+    sync::mpsc::{Receiver, Sender},
+};
+
+pub async fn create_connection_manager<Req: Communicable, Resp: Communicable>(
+    stream: TcpStream,
+    mut rx: Receiver<Req>,
+    tx: Sender<Resp>,
+) {
+    let (mut reader, mut writer) = io::split(stream);
+    loop {
+        tokio::select! {
+            request = rx.recv() => {
+                let Some(request) = request else { break; };
+                let _ = stream_serialization::send(&request, &mut writer).await;
+            },
+            response = stream_serialization::receive::<Resp>(&mut reader) => {
+                let Ok(response) = response else { break; };
+                let _ = tx.send(response).await;
+            },
+        }
+    }
+}
 
 // Will not send a new request until it receives a response from the previous request.
 pub async fn create_cyclic_connection_manager<S: Communicable, R: Communicable>(
