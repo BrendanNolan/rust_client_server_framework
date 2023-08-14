403786f11e81faf3e8bfcae7b0e1254846252d22
Brendan Nolan <bnolan23@gmail.com>
2023-08-14 08:39:34 +0100

    Rename ReadError->StreamingError

--- connection_utils/src/stream_serialization.rs
+++ connection_utils/src/stream_serialization.rs
@@ -13,20 +13,20 @@ struct ByteMessage {
     data: ByteArray,
 }
 
-pub enum ReadError {
+pub enum StreamingError {
     IoError(tokio::io::Error),
     DeserializationError(bincode::Error),
 }
 
-impl From<bincode::Error> for ReadError {
+impl From<bincode::Error> for StreamingError {
     fn from(error: bincode::Error) -> Self {
-        ReadError::DeserializationError(error)
+        StreamingError::DeserializationError(error)
     }
 }
 
-impl From<tokio::io::Error> for ReadError {
+impl From<tokio::io::Error> for StreamingError {
     fn from(error: tokio::io::Error) -> Self {
-        ReadError::IoError(error)
+        StreamingError::IoError(error)
     }
 }
 
@@ -39,7 +39,7 @@ fn convert_to_byte_message(item: &impl Communicable) -> ByteMessage {
 pub async fn send(
     data: &impl Communicable,
     writer: &mut WriteHalf<TcpStream>,
-) -> Result<(), ReadError> {
+) -> Result<(), StreamingError> {
     let byte_message = convert_to_byte_message(data);
     writer.write_u64(byte_message.size).await?;
     writer.write_all(&byte_message.data).await?;
@@ -48,7 +48,7 @@ pub async fn send(
 
 pub async fn receive<R: DeserializeOwned>(
     reader: &mut ReadHalf<TcpStream>,
-) -> Result<R, ReadError> {
+) -> Result<R, StreamingError> {
     let num_bytes_to_read = reader.read_u64().await?;
     let mut raw_bytes_received = ByteArray::new();
     raw_bytes_received.resize(num_bytes_to_read as usize, 0_u8);
